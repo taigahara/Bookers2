c1013f86f21b71d86467910724624fb764cd6397
原　大河 <>
2020-08-23 11:13:01 +0900

    dmmbookers2

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '3ca2b91f672490e3ad2655ef96120d7259535f11f90d6afc779675cb92431dff07841e0eb73354a4109eb93599800a58284c454a92f8d6ecf3dd99c5c331f1f2'
